aed71ce32fea42755e9630ef194f18f216b53d83
Diwas Atreya <74746579+diwasatreya@users.noreply.github.com>
2024-02-11 17:41:40 +0545

    Delete GIF Logo directory

--- GIF Logo/events/Client/shardDisconnect.js
+++
@@ -1,8 +0,0 @@
-const { MessageEmbed } = require('discord.js');
-module.exports = {
-  name: "shardDisconnect",
-  run: async (client, event, id) => {
-    
-  client.logger.log(`Shard #${id} Disconnected`, "warn");
-  }
-};
