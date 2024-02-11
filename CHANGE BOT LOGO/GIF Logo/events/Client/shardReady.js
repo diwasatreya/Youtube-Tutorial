d285b8a15ef4244d67170de03819029e862c6967
Diwas Atreya <74746579+diwasatreya@users.noreply.github.com>
2024-02-11 17:22:13 +0545

    Delete CHANGE BOT LOGO directory

--- CHANGE BOT LOGO/GIF Logo/events/Client/shardReady.js
+++
@@ -1,8 +0,0 @@
-
-const { MessageEmbed } = require('discord.js');
-module.exports = {
-  name: "shardReady",
-  run: async (client, id) => {
-  client.logger.log(`Shard #${id} Ready`, "ready");
-  }
-};
