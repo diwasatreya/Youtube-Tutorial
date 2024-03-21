fdcb508e962e893f4509010e70b2405a669274a6
zyrxx <162786329+zyrxx@users.noreply.github.com>
2024-03-22 02:35:10 +0530

    config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   creator: "zy3x.",
-  token: process.env.TOKEN || 'MTIxNTY5MzUwODc0MDkxMTIwNg.Gh2CQq.v3n_v0Mpl-TLFltRkjjqf_fYgjwlylre8KSjU8',
+  token: process.env.TOKEN || '',
   prefix: process.env.PREFIX || '!', 
   ownerID: process.env.OWNERID?.split(',') || ['1175829774086717444'], 
   embedColor: process.env.COlOR || '#2b2d31',
